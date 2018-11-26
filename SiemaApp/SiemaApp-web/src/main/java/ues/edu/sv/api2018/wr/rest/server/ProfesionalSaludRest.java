4332004065f5ef8f0a55f9978fd2cd5b02ad9732
Ronald <ronald_reir21@hotmail.com>
2018-11-26 17:43:51 -0600

    se arreglo el metodo count en la entidad ProfesionalSaludRest

--- SiemaApp/SiemaApp-web/src/main/java/ues/edu/sv/api2018/wr/rest/server/ProfesionalSaludRest.java
+++ SiemaApp/SiemaApp-web/src/main/java/ues/edu/sv/api2018/wr/rest/server/ProfesionalSaludRest.java
@@ -13,12 +13,14 @@ import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import ues.edu.api2018.entities.ProfesionalSalud;
 import ues.edu.api2018.sessions.ProfesionalSaludFacadeLocal;
 
@@ -49,21 +51,42 @@ public class ProfesionalSaludRest implements Serializable {
         }
         return salida;
     }
-
-    @Path("count")
+    
     @GET
-    @Produces({MediaType.TEXT_PLAIN})
-    public Integer count() {
-
-        try {
-            if (profesionalSaludFacade != null) {
-                return profesionalSaludFacade.count();
+    @Path("count")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response count() {
+        if (profesionalSaludFacade != null) {
+            try {
+                int totalRegistros = profesionalSaludFacade.count();
+                if (totalRegistros != 0) {
+                    return Response.status(Response.Status.OK)
+                            .entity(totalRegistros).build();
+                }
+            } catch (Exception e) {
             }
 
-        } catch (Exception e) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
         }
-        return 0;
+        return Response.status(Response.Status.NOT_FOUND)
+                .header("No se encontraron Registros", profesionalSaludFacade)
+                .build();
     }
 
+    /*
+   @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response findId(@PathParam("id") Integer identificador) {    
+
+         ProfesionalSalud buscado = profesionalSaludFacade.find(identificador);
+
+        try {
+            if (identificador >= 0 && buscado != null) {
+                return Response.status(Response.Status.OK).entity(buscado).build();
+            }
+        } catch (Exception e) {
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).header("no se encontro registro:", identificador).build();
+    }*/
 }
