c40e6d31239d1cb92a6a5d30ef83a4d37afcab15
danielM9521 <daniel_murillo.95@hotmail.com>
2018-11-27 14:10:10 -0600

    Ya funciona 2.0

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose down
-cp SiemaApp/SiemaApp-ear/target/SiemaApp-ear-1.0-SNAPSHOT/SiemaApp-web-1.0-SNAPSHOT.war Payara/
+cp SiemaApp/SiemaApp-ear/target/SiemaApp-ear-1.0-SNAPSHOT.ear Payara/
 docker rmi payarafullapi
 docker-compose up
